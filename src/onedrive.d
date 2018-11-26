0b36b26fb9c88589fb10ac9b776aa766ddc5af61
Norbert Preining <norbert@preining.info>
2018-11-26 09:57:42 +0900

    New upstream version 2.2.0

--- src/onedrive.d
+++ src/onedrive.d
@@ -14,10 +14,14 @@ private immutable {
 	string authUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
 	string redirectUrl = "https://login.microsoftonline.com/common/oauth2/nativeclient";
 	string tokenUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
-	string driveUrl = "https://graph.microsoft.com/v1.0/me/drive";
-	string itemByIdUrl = "https://graph.microsoft.com/v1.0/me/drive/items/";
-	string itemByPathUrl = "https://graph.microsoft.com/v1.0/me/drive/root:/";
 	string driveByIdUrl = "https://graph.microsoft.com/v1.0/drives/";
+}
+
+private {
+    string driveUrl = "https://graph.microsoft.com/v1.0/me/drive";
+    string itemByIdUrl = "https://graph.microsoft.com/v1.0/me/drive/items/";
+    string itemByPathUrl = "https://graph.microsoft.com/v1.0/me/drive/root:/";
+    string driveId = "";
 }
 
 class OneDriveException: Exception
@@ -71,6 +75,15 @@ final class OneDriveApi
 
 	bool init()
 	{
+		try {
+			driveId = cfg.getValue("drive_id");
+			if (driveId.length) {
+				driveUrl = driveByIdUrl ~ driveId;
+                itemByIdUrl = driveUrl ~ "/items";
+                itemByPathUrl = driveUrl ~ "/root:/";
+			}
+		} catch (Exception e) {}
+	
 		try {
 			refreshToken = readText(cfg.refreshTokenFilePath);
 		} catch (FileException e) {
@@ -329,8 +342,9 @@ final class OneDriveApi
 		if (!skipToken) addAccessTokenHeader(); // HACK: requestUploadStatus
 		auto response = perform();
 		checkHttpCode(response);
+		// OneDrive API Response Debugging
 		if (.debugResponse){
-			log.vlog("OneDrive API Response: ", response);
+			writeln("OneDrive API Response: ", response);
         }
 		return response;
 	}
@@ -481,7 +495,7 @@ final class OneDriveApi
 			content ~= data;
 			// HTTP Server Response Code Debugging
 			if (.debugResponse){
-				log.vlog("OneDrive HTTP Server Response: ", http.statusLine.code);
+				writeln("OneDrive HTTP Server Response: ", http.statusLine.code);
 			}
 			
 			return data.length;
